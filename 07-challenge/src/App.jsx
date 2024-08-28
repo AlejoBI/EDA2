8df85ac985e7305e142fd9f654c941613e91654c
Alejandro Bravo Isajar <alejandrobravoisajar1@gmail.com>
2024-08-28 15:15:15 -0500

    Challenge 07- inicio de react con vite

---
+++ 07-challenge/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <>
+      <h1>
+        07-challenge
+      </h1>
+      <span style={{
+        fontSize: 30
+      }}>
+        Texto de prueba: 10
+      </span>
+    </>
+  )
+}
+
+export default App
