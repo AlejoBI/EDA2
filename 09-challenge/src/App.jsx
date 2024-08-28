44b0babfe9c71e013bbe5585e370ab25812a92b5
Alejandro Bravo Isajar <alejandrobravoisajar1@gmail.com>
2024-08-28 16:10:56 -0500

    Refactor

--- 09-challenge/src/App.jsx
+++ 09-challenge/src/App.jsx
@@ -1,41 +1,35 @@
-import './App.css'
-import { useState } from 'react'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [categories, setCategories] = useState([])
-  const [category, setCategory] = useState('')
+  const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState("");
 
   const addCategory = () => {
-    setCategories([...categories, category])
-    setCategory('')
-  }
+    setCategories([...categories, category]);
+    setCategory("");
+  };
 
   const setCategoryValue = (e) => {
-    setCategory(e.target.value)
-  }
+    setCategory(e.target.value);
+  };
 
   return (
     <>
-      <h1>
-        Add Categories
-      </h1>
-      <input type="text" value={category} onChange={
-        (e) => setCategoryValue(e)
-      }/>
-      <button onClick={
-        () => addCategory()
-      }>
-        addCategorys
-      </button>
+      <h1>Add Categories</h1>
+      <input
+        type="text"
+        value={category}
+        onChange={(e) => setCategoryValue(e)}
+      />
+      <button onClick={() => addCategory()}>addCategorys</button>
       <ul>
-        {
-          categories.map((category, index) => {
-            return <li key={index}>{category}</li>
-          })
-        }
+        {categories.map((category, index) => {
+          return <li key={index}>{category}</li>;
+        })}
       </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
