339a7d05b3e55cd42c9afdaf157c0eb8aba39620
Alejandro Bravo Isajar <alejandrobravoisajar1@gmail.com>
2024-09-11 22:03:02 -0500

    11-challenge

---
+++ 11-challenge/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Count from './components/Count'
+
+function App() {
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <Count />
+    </>
+  )
+}
+
+export default App
---
+++ 11-challenge/src/components/Count.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const Count = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="Count">
+      <h2>{count}</h2>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
+
+export default Count;
